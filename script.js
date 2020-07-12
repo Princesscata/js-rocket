c937dcfe539318abf7720447b8a8f040004c1252
Princesscata <67717110+Princesscata@users.noreply.github.com>
2020-07-12 12:38:36 -0400

    add

---
+++ script.js
@@ -0,0 +1,18 @@
+var car = {
+  make: 'VW',
+  type: 'polo',
+  color: 'blue',
+  isTurnedOn: false,
+  numerOfWheels: 4,
+  seats: [
+    'seat 1',
+    'seat 2',
+    'seat 3',
+    'seat 4'
+  ],
+  turnOn: function (){
+    this.isTurnedOn = true;
+  }
+};
+
+console.log("hello there friends!");
